dab3d6803cda114aa6ffac717e8d91ebe851b9e7
Mateusz Sznura <mateusz.sznura@stxnext.pl>
2018-11-17 14:08:18 +0100

    collapse app config

--- src/AppConfig.js
+++ src/AppConfig.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { 
   Button,
+  Collapse,
   Form,
   FormGroup,
   FormText,
@@ -12,22 +13,38 @@ const DEFAULT_API_KEY = '6c26db6dd08d98b1d99b2f77c7a88c90';
 
 class AppConfig extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: false,
+    }
+    this.toggleCollapse = this.toggleCollapse.bind(this);
+  }
+
+  toggleCollapse() {
+    this.setState(state => ({
+      collapse: !state.collapse,
+    }));
+  }
+
   render() {
     return (
       <div className="appConfig">
         <h1>App Config</h1>
-        <Button color="primary">Show</Button>
-        <Form>
-          <FormGroup>
-            <Label for="api-key-input">API Key</Label>
-            <Input type="string" id="api-key-input" defaultValue={DEFAULT_API_KEY} />
-            <FormText color="muted">
-              This app uses fixer.io api and thus requires fixer.io API key. 
-              You can use default, but if the monthly request limit is exceeded,
-              you can create your own free fixer.io account and provide own API key.
-            </FormText>
-          </FormGroup>
-        </Form>
+        <Button color="primary" onClick={this.toggleCollapse}>Show</Button>
+        <Collapse isOpen={this.state.collapse}>
+          <Form>
+            <FormGroup>
+              <Label for="api-key-input">API Key</Label>
+              <Input type="string" id="api-key-input" defaultValue={DEFAULT_API_KEY} />
+              <FormText color="muted">
+                This app uses fixer.io api and thus requires fixer.io API key.
+                You can use default, but if the monthly request limit is exceeded,
+                you can create your own free fixer.io account and provide own API key.
+              </FormText>
+            </FormGroup>
+          </Form>
+        </Collapse>
       </div>
     );
   }
